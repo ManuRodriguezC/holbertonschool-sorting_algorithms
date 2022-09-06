ddcaea0d5640269c4a9efd01854c1bb2d273311d
ManuRodriguezC <estebanclimb@gmail.com>
2022-09-06 12:17:48 -0500

    Add to the function to sort the duble linked list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,7 +10,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *temp = *list;
 	listint_t *before, *current;
 
-	if (!list || !list->next)
+	if (!list || (*list)->next == NULL)
 		return;
 
 	while (temp)
