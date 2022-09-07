9a651e0687e16172fbf69e8825c9933e34764f38
ManuRodriguezC <estebanclimb@gmail.com>
2022-09-07 11:13:46 -0500

    Function that sort to the algorimt shell

---
+++ 100-shell_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * shell_sort - Function that sorts an array of integers in ascending
+ * order using the Shell sort algorithm, using the Knuth sequence
+ * @array: The array to sort.
+ * @size: Size of the array
+ */
+void shell_sort(int *array, size_t size)
+{
+	if (!array || size == 0)
+		return;
+
+	size_t med = 1, i, j;
+	int temp;
+
+	for (med = med * 3 + 1; med > 0; med /= 3)
+	{
+	for (i = med; i < size; i++)
+
+	{
+		temp = array[i];
+		for (j = i; j >= med && array[j - med] > temp; j -= med)
+		{
+			array[j] = array[j - med];
+		}
+		array[j] = temp;
+	}
+	print_array(array, size);
+	}
+}
--- sort.h
+++ sort.h
@@ -29,6 +29,7 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 /* prototypes of quick sort */
 void quick_sort(int *array, size_t size);
